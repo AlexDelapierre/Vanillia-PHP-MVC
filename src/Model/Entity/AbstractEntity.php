157812cf6ba2a5a94553b109f638f9b9d4db5088
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-04-18 18:46:40 +0200

    feat: add AbstractEntity class for base entity functionality

---
+++ src/Model/Entity/AbstractEntity.php
@@ -0,0 +1,35 @@
+<?php
+
+
+abstract class AbstractEntity
+{
+    // ?int signifie qu'il peut être null. Si null = nouveau, si entier = existe en BDD.
+    protected ?int $id = null;
+
+    public function __construct(array $data = [])
+    {
+        if (!empty($data)) {
+            $this->hydrate($data);
+        }
+    }
+
+    protected function hydrate(array $data): void
+    {
+        foreach ($data as $key => $value) {
+            // Transformation de user_id en setUserId
+            $method = 'set' . str_replace('_', '', ucwords($key, '_'));
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+}
